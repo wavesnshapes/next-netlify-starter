ab8522fd91e250a1ddaeaaaa1f391ec414d10447
wavesnshapes <81838113+wavesnshapes@users.noreply.github.com>
2021-04-03 13:40:41 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Selamlar" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          buralar hiç bilmiyorum ben ama 1-2 haftaya bişeyler çıkmaya başlar :*
         </p>
       </main>
 
